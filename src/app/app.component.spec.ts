f198fbcfe2c681d7dedf4dca6434fd259675c786
ThomasCalveteRuffier <thomas.calveteruffier@gmail.com>
2023-03-09 15:41:38 +0100

    Calculatrice

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,39 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'calculatrice'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('calculatrice');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('calculatrice app is running!');
+  });
+
+  it(`Ca doit me faire une addition`, () => {
+    expect(component.add(2, 3)).toBe(5);
+  });
+
+  it('Ca doit me faire une soustraction', () => {
+    expect(component.sub(8, 5)).toBe(3);
+  });
+
+  it('Ca doit me faire une multiplication', () => {
+    expect(component.mul(4, 3)).toBe(12);
+  });
+
+  it('Ca doit me faire une division', () => {
+    expect(component.div(9, 3)).toBe(3);
+  });
+
+  it('Ca doit etre une erreur', () => {
+    expect(() => component.div(9, 0)).toThrow();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'calculatrice';
+
+  add(a: number, b: number) {
+    return a + b;
+  }
+
+  sub(a: number, b: number) {
+    return a - b;
+  }
+
+  mul(a: number, b: number) {
+    return a * b;
+  }
+
+  div(a: number, b: number) {
+    if (b === 0) {
+      throw new Error();
+    }
+    return a / b;
+  }
 }
